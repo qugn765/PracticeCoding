062d8e438c5045721d4645588a55aefcd663c3f5
qugn765 <87963035+qugn765@users.noreply.github.com>
2023-07-23 15:00:49 +0900

    [level 0] Title: 옹알이 (1), Time: 1.34 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] arr = {"aya","ye","woo","ma"};
+        for(int i = 0 ; i < babbling.length; i++){  
+            for(int j = 0; j <arr.length; j++){
+                babbling[i] = babbling[i].replaceAll(arr[j]," ");
+            }
+            babbling[i] = babbling[i].trim();
+            if(babbling[i].equals("") || babbling[i] == null) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
