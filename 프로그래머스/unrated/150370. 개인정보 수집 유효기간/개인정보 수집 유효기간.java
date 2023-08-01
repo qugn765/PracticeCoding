3f56495d434ff4f56dc994b08a56f932a968bb09
qugn765 <87963035+qugn765@users.noreply.github.com>
2023-08-01 13:45:39 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 4.70 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        Map<String, Integer> map1 = new HashMap<>();
+        List<Integer> list1 = new ArrayList<>();
+        String[] today_arr = today.split("\\.");
+        System.out.print(Arrays.toString(today_arr));
+        int today_year = Integer.parseInt(today_arr[0]);
+        int today_month = Integer.parseInt(today_arr[1]);
+        int today_day = Integer.parseInt(today_arr[2]);
+        for(int i = 0; i < terms.length; i++){
+            String[] arr = terms[i].split(" ");
+            map1.put(arr[0], Integer.parseInt(arr[1]));
+        }
+        
+        for(int i = 0; i < privacies.length; i++){
+            String[] p_arr = privacies[i].split(" ");
+            String[] p_date = p_arr[0].split("\\.");
+            
+            int plus_month = map1.get(p_arr[1]);
+            int plus_year = 0;
+            int day = Integer.parseInt(p_date[2]);
+            int month = Integer.parseInt(p_date[1]) + plus_month;
+            while(month > 12){
+                month = month - 12;
+                plus_year++;
+            }
+            int year = Integer.parseInt(p_date[0]) + plus_year;
+            if(year < today_year){
+                list1.add(i + 1);
+            } else if(year > today_year){
+                continue;
+            } else if(year == today_year) {
+                if(month < today_month){
+                    list1.add(i + 1);
+                } else if(month > today_month){
+                    continue;
+                } else if(month == today_month) {
+                    if(day < today_day){
+                        list1.add(i + 1);
+                    } else if(day > today_day){
+                        continue;
+                    } else if(day == today_day) {
+                        list1.add(i + 1);
+                    }
+                }
+            }
+        }
+        int[] answer = list1.stream().mapToInt(i->i).toArray();
+        return answer;
+    }
+}
